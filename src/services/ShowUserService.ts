6a6dbfc8f31fe2e88a81ba596019e783dbbbacc5
blvkroot <hsalukamba17@gmail.com>
2023-06-12 14:52:39 +0200

    feat: :sparkles: ShowUserService created

---
+++ src/services/ShowUserService.ts
@@ -0,0 +1,15 @@
+import { injectable } from "tsyringe";
+import { users } from "..";
+import ErrorsApp from "../utils/ErrorsApp";
+import { UserDTO } from "dto/UserDTO";
+
+@injectable()
+export class ShowUserService {
+  async execute(name: string): Promise<UserDTO> {
+    const user = users.find((user) => user.name == name);
+
+    if (!user) throw new ErrorsApp("User not found");
+
+    return user;
+  }
+}
