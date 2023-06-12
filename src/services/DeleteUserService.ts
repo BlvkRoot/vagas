a227f0e64845314865c6c632648d383f20b19764
blvkroot <hsalukamba17@gmail.com>
2023-06-13 00:01:34 +0200

    refactor: :recycle: Delete user refactored

--- src/services/DeleteUserService.ts
+++ src/services/DeleteUserService.ts
@@ -1,6 +1,5 @@
 import { injectable } from "tsyringe";
-import { users } from "..";
-import ErrorsApp from "../utils/ErrorsApp";
+import { permissions, users } from "..";
 import { BaseUserService } from "./BaseUserService";
 
 @injectable()
@@ -8,5 +7,8 @@ export class DeleteUserService extends BaseUserService {
   async execute(id: number): Promise<void> {
     const findIndex = this.getUserIndexById(id);
     users.splice(findIndex, 1);
+    // Remover permissão caso usuário foi deletado
+    permissions.can_delete = permissions.can_delete.filter(userId => userId != id);
+    permissions.can_update = permissions.can_update.filter(userId => userId != id);
   }
 }
